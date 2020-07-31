3f9afd2b346bec9a6eaac07ab5c831b852a676a6
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-07-31 14:54:43 +0200

    created action nais applications

---
+++ cucumber-apps/apps.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+# fuksjon som henter ut et github prosjekt for en applikasjon (github prosjekt)
+function checkoutNais() {
+  NAIS_APP=$1
+
+  if [[ "$GITHUB_REF" != "refs/heads/master" ]]; then
+    FEATURE_BRANCH=${GITHUB_REF#refs/heads/}
+    # shellcheck disable=SC2046
+    IS_POSSIBLE_API_CHANGE=$(git ls-remote --heads $(echo "https://github.com/navikt/$NAIS_APP $FEATURE_BRANCH" | sed "s/'//g") | wc -l)
+
+    if [[ $IS_POSSIBLE_API_CHANGE -eq 1 ]]; then
+      echo "Using feature branch: $FEATURE_BRANCH"
+      git clone --depth 1 --branch="$FEATURE_BRANCH" "https://github.com/navikt/$NAIS_APP"
+    else
+      git clone --depth 1 "https://github.com/navikt/$NAIS_APP"
+    fi
+  else
+    git clone --depth 1 "https://github.com/navikt/$NAIS_APP"
+  fi
+}
+
+############################################
+#
+# Følgende forutsetninger for dette skriptet
+# - input til skriptet er applikasjonsnavn (github prosjekt navn)
+#
+# Følgende skjer i dette skriptet:
+# 1) sjekker og setter input
+# 2) sletter eventuell eksisterende mappe og lager ny mappe som brukes til å hente disse prosjektene
+# 3) for hvert applikasjonsnavn (github prosjekt)
+#    - sjekker ut prosjektet
+# 4) setter mappa med <applikasjoner> (full filsti) som output
+#
+############################################
+
+if [[ $# -eq 0 ]]; then
+  echo "Bruk: apps.sh <github project>...<another github project>"
+  exit 1;
+fi
+
+# shellcheck disable=SC2124
+NAIS_APPS=$@
+
+if [[ -d nais-apps ]]; then
+  sudo rm -rf nais-apps
+fi
+
+mkdir nais-apps
+cd nais-apps || exit 1
+
+for app in $NAIS_APPS
+do
+  checkoutNais "$app"
+done
+
+echo ::set-output name=nais-apps-folder::"$PWD/nais-apps"
